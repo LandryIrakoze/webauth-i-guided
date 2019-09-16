328e8b88497324ff5932e4e96b1414eaf543feae
Landry Irakoze <irakozel@gmail.com>
2019-09-16 13:45:23 -0400

    validator middleware

--- index.js
+++ index.js
@@ -67,3 +67,33 @@ server.get('/has', (req, res) => {
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+/*
+  write a middleware that will check for the username and password 
+  let the request continue to /api/users if credentials are good
+  return a 401 if credentials are invalid
+
+  use the middleware to restrict access to GET /api/users endpoint
+  remember (req, res, next)
+*/
+
+function validator(res, res, next) {
+  let { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+  } else {
+    res.status(400).json({ message: 'No Credentials Provided' })
+  }
+}
